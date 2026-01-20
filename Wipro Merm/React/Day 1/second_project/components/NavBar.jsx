2ed6f6474aac42e7ef4b2db21b5b96d3865e6211
Aishvarya-Dhevi <93396965+Aishvarya-Dhevi@users.noreply.github.com>
2026-01-20 17:22:15 +0530

    Add files via upload

---
+++ Wipro Merm/React/Day 1/second_project/components/NavBar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h2 className="logo">Great Learning</h2>
+
+      <ul className="menu">
+        <li>Home</li>
+        <li>Courses</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
